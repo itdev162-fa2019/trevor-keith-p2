02659f9c1b85eb744b1e0ed2fcd3e0cc1d3e71ef
Trevor Keith <keithtw@gmatc.matc.edu>
2019-12-14 20:38:52 -0600

    Input fields

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Header from './components/layout/header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 class  App extends Component {
   state = {
@@ -47,7 +49,13 @@ class  App extends Component {
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} toggleComplete={this.toggleComplete} delTodo={this.delTodo} />
+        <div className="container">
+          <Header />
+          <AddTodo />
+          <Todos todos={this.state.todos} toggleComplete={this.toggleComplete} delTodo={this.delTodo} />
+
+        </div>
+        
       </div>
 
     );
