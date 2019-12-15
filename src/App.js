841acf6ba8813e30eedea9f7da51dcdd37bce19c
Trevor Keith <keithtw@gmatc.matc.edu>
2019-12-14 18:53:38 -0600

    0

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Todos from './components/Todos';
+
+class  App extends Component {
+  state = {
+    todos: [
+      {
+      id: -1,
+      title: "Work",
+      completed: false
+      },
+      {
+        id: -2,
+        title: "Make dinner",
+        completed: false
+      },
+      {
+        id: -3,
+        title: "Eat dinner",
+        completed: false
+      }
+
+    ]
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+
+    );
+
+  }
+  
+}
+
+export default App;
